c9ff009b2aa67423a3b36c8abc42a2e9cad0f431
Rabeea Shahid <134997377+Rabeea-S@users.noreply.github.com>
2026-02-19 00:08:26 -0700

    added q5

---
+++ linked_list_question5/target/debug/deps/linked_list_question5.d
@@ -0,0 +1,5 @@
+C:\Users\rabee\421\assignment4\linked_list_question5\target\debug\deps\linked_list_question5.d: src\main.rs
+
+C:\Users\rabee\421\assignment4\linked_list_question5\target\debug\deps\linked_list_question5.exe: src\main.rs
+
+src\main.rs:
---
+++ linked_list_question5/target/debug/linked_list_question5.d
@@ -0,0 +1,1 @@
+C:\Users\rabee\421\assignment4\linked_list_question5\target\debug\linked_list_question5.exe: C:\Users\rabee\421\assignment4\linked_list_question5\src\main.rs
